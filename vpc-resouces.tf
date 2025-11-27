59f639b91eadbab27fb7b102ca39eaffa9c718a7
manithota1211 <thotamani39@gmail.com>
2025-11-27 17:31:39 +0530

    terraform main.tf file added

---
+++ vpc-resouces.tf
@@ -0,0 +1,86 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "main_vpc"
+    }
+    }
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+        Name = "public_subnet"
+    }
+
+}
+
+resource "aws_subnet" "private_subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+
+    tags = {
+        Name = "private_subnet"
+    }
+
+}
+resource "aws_internet_gateway" "main_igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "internet_gateway"
+    }
+
+}
+resource "aws_route_table" "public_route" {
+    vpc_id = aws_vpc.main.id
+      route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main_igw.id
+      }
+
+    tags = {
+        Name = "public_route_table"
+    }
+   
+}
+resource "aws_route_table" "private_route" {
+    vpc_id = aws_vpc.main.id
+      route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gw.id
+      }
+    tags = {
+      Name = "private_route_table"
+  }
+        
+}
+
+
+resource "aws_route_table_association" "public_subnet" {
+    subnet_id = aws_subnet.public_subnet.id
+    route_table_id = aws_route_table.public_route.id
+}
+
+
+resource "aws_route_table_association" "private_subnet_bar" {
+    subnet_id = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private_route.id
+}
+
+resource "aws_eip" "nat_ip" {
+    tags = {
+        Name = "nat_eip"
+
+    
+    }
+}
+resource "aws_nat_gateway" "nat_gw" {
+    allocation_id = aws_eip.nat_ip.id
+    subnet_id = aws_subnet.public_subnet.id
+
+    tags = {
+        Name = "nat_gateway"
+    }
+}
